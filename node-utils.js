e9c89f46826c309aef43a5450604b7009517c31a
Ismael Nieto <ismael.nieto@gmail.com>
2021-03-24 12:52:26 +0100

    Exercises 2 and 3 of Level 1 ready

--- node-utils.js
+++ node-utils.js
@@ -16,3 +16,30 @@ const recursiveFunction = () => {
     }
 } 
 recursiveFunction();
+
+/* - Exercici 2
+Creu una funció que, en executar-la, escrigui el seu nom en un fitxer. */
+
+const fs = require('fs')
+
+const writeFunctionName = () => {
+
+    fs.writeFile("./texto.txt", "printFuncionName", (error)=>{
+        if (error) throw error;
+        console.log("Name was succesfully written");
+    });
+}
+writeFunctionName();
+
+/* - Exercici 3
+Creu una altra que imprimeixi per pantalla el que llegeixi d'un fitxer. */
+
+const outputFileContent = () => {
+
+    fs.readFile("./texto.txt",  (error, data)=>{
+        if (error) throw error;
+        console.log(`Following content was found in the file: ${data}.`);
+
+    });
+}
+outputFileContent();
