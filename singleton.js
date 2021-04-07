8a632a3f59718081f99527c13d9d50f043669225
Ismael Nieto <ismael.nieto@gmail.com>
2021-04-07 12:03:18 +0200

    Singleton exercise works

--- singleton.js
+++ singleton.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 /* Nivell 2
 - Exercici 1 - Singleton
-Construeixi una aplicació que creï diversos jugadors. Els jugadors podran ser afegits a un Joc, que mostrarà un marcador amb les puntuacions i el guanyador.
- Podrà fixar en cada jugador punts guanyats o perduts perquè el marcador canviï. 
+Construeixi una aplicació que creï diversos jugadors. Els jugadors podran ser afegits a un Joc, 
+que mostrarà un marcador amb les puntuacions i el guanyador.
+ Podrà fixar en cada jugador punts guanyats o perduts perquè el marcador canvï. 
 La classe Marcador deurà, com a requisit indispensable, implementar un patró Singleton. */
 
 // Load modules
@@ -19,15 +20,27 @@ class Player{
 let score = null;
 // Create ScoreBoard: singleTon pattern
 class ScoreBoard{
-    constructor(firstScore, secondScore, winner) {
+    constructor(playerOne, playerTwo, winner) {
         if (!score) {
-            this.firstScore = firstScore;
-            this.secondScore = secondScore;
+            this.playerOne = playerOne.name;
+            this.playerTwo = playerTwo.name;
+            this.firstScore = playerOne.score;
+            this.secondScore = playerTwo.score;
             this.winner = winner;
             score =  this;
         } else {
             return score;
         }
+    }//End constructor
+    getWinner(){
+        if (this.firstScore > this.secondScore){
+            this.winner = this.playerOne;
+        } else if (this.firstScore < this.secondScore){
+            this.winner = this.playerTwo;
+        } else {
+            this.winner = "Nobody";
+        }
+        console.log(`Game Over. ${this.winner} wins. ${this.firstScore} to ${this.secondScore}}!`)
     }
 }
 
@@ -35,9 +48,41 @@ class ScoreBoard{
 class Game{
     constructor(score, firstPlayer, secondPlayer){
         this.score = score;
-        this.firstPlayer = firstPlayer;
-        this.secondPlayer = secondPlayer;
+        this.firstPlayer = firstPlayer.name;
+        this.secondPlayer = secondPlayer.name;
     }
+
+    start(){
+        let counter = 0;
+        // Set rounds to play
+        const rounds = +prompt("Let's Play! How many rounds would you like to play? ");
+        // Repeat so often like rounds
+        while (counter < rounds){
+            ++counter;
+            // Random a number to be guessed by Players
+            let numberToGuess = Math.floor(Math.random() * 3)
+            console.log(`This is the number to guess: ${numberToGuess}`);
+            // Enter Player choices
+            let firstChoice = +prompt(`${this.firstPlayer}, please enter a number between 0 and 3: `);
+            let secondChoice = +prompt(`${this.secondPlayer}, please enter a number between 0 and 3: `);
+            //Check result and modify scores
+            if (firstChoice === numberToGuess && secondChoice !== numberToGuess){
+                console.log(`${this.firstPlayer} wins this round! 100 Points. `)
+                this.score.firstScore += 100;
+            } else if (firstChoice !== numberToGuess && secondChoice === numberToGuess){
+                console.log(`${this.secondPlayer} wins this round! 100 Points `)
+                this.score.secondScore += 100;
+            } else if (firstChoice === numberToGuess && secondChoice === numberToGuess){
+                console.log(`Path situation. Both players get 50 Points. `)
+                this.score.firstScore += 50;
+                this.score.secondScore += 50;
+            } else {
+                console.log(`Nobody wins. Both players get 0 Points. `)
+            }
+        } // End While-loop
+        console.log( this.score.firstScore, this.score.secondScore)
+        this.score.getWinner();
+    }// End start method
 }
 
 // Create playerFactory: Factory pattern
@@ -52,15 +97,15 @@ const players = new PlayerFactory();
 
 // Application starts
 //Enter players
-
-const namePlayer1 = prompt('What is your name?');
+const namePlayer1 = prompt('Name of first player: ');
 const player1 = players.createPlayers(namePlayer1);
-const namePlayer2 = prompt('Please enter also your name?');
+const namePlayer2 = prompt('Name of second player: ');
 const player2 = players.createPlayers(namePlayer2);
 
 //Starts game
-const scoreGame = new ScoreBoard(0, 0, "pending");
+const scoreGame = new ScoreBoard(player1, player2, "pending");
 const newGame = new Game(scoreGame, player1, player2)
 
 
-console.log(newGame);
+//console.log(newGame);
+newGame.start()
